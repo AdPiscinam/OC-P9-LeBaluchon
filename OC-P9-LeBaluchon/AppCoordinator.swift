2dfd131f7cd8885d76c2d3ad282ccf8e465ff2f1
AdPiscinam <lab@walimaloui.com>
2022-04-08 11:54:42 +0200

    Add App Coordinator and Main Tab View Controller

---
+++ OC-P9-LeBaluchon/AppCoordinator.swift
@@ -0,0 +1,29 @@
+// AppCoordinator.swift
+// OC-P9-LeBaluchon
+// Created by Ad Piscinam on 08/04/2022
+// 
+
+import UIKit
+
+protocol Coordinator: AnyObject {
+    var navigationController: UINavigationController {get set}
+    var childCoordinators: [Coordinator] {get set}
+    func start()
+}
+
+class AppCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    
+    var childCoordinators: [Coordinator] = []
+        
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+}
+
+extension AppCoordinator {
+    func start() {
+    }
+}
+
---
+++ OC-P9-LeBaluchon/MainTabBarController.swift
@@ -0,0 +1,28 @@
+// MainTabBarController.swift
+// OC-P9-LeBaluchon
+// Created by Ad Piscinam on 08/04/2022
+// 
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    let appCoordinator = AppCoordinator(navigationController: UINavigationController())
+    
+    let converterCoordinator = ConverterCoordinator(navigationController: UINavigationController())
+    let weatherCoordinator = WeatherCoordinator(navigationController: UINavigationController())
+    let translatorCoordinator = TranslatorCoordinator(navigationController: UINavigationController())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        appCoordinator.start()
+        converterCoordinator.start()
+        viewControllers = [appCoordinator.navigationController, converterCoordinator.navigationController]
+    }
+    
+    
+    
+    
+    
+}
