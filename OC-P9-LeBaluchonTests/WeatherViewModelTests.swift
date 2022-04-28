ba2c4384fbab14d8ae93b15758829e19f6268ee2
AdPiscinam <lab@walimaloui.com>
2022-04-28 05:46:27 +0200

    Create and add Weather View Model Unit Tests

---
+++ OC-P9-LeBaluchonTests/WeatherViewModelTests.swift
@@ -0,0 +1,112 @@
+// WeatherViewModelTests.swift
+// OC-P9-LeBaluchon
+// Created by Ad Piscinam on 28/04/2022
+// 
+
+import XCTest
+@testable import OC_P9_LeBaluchon
+
+class WeatherViewModelTests: XCTestCase {
+    
+    
+    var sut: WeatherViewModel!
+    
+    override func setUpWithError() throws {
+        //TODO:
+        let mock = MockWeatherNetwork()
+        sut = WeatherViewModel(network: mock)
+    }
+    
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func testGivenAWeatherViewModel_WhenViewDidLoad_ThenTitleIsCorrectlyReturned() {
+        let expectation = self.expectation(description: "Title is not Correct")
+        let expectedTitle: String = "Weather"
+        sut.titleText = { title in
+            XCTAssertEqual(title, expectedTitle)
+            expectation.fulfill()
+        }
+        sut.viewDidLoad()
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    
+    func testGivenAWeatherViewModel_WhenViewDidLoad_ThenNYNameIsCorrectlyReturned() {
+        let expectation = self.expectation(description: "NY City Name is not Correct")
+        let expectedName: String = "New York"
+        sut.nyCityNameUpdater = { name in
+            XCTAssertEqual(name, expectedName)
+            expectation.fulfill()
+        }
+        sut.viewDidLoad()
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    
+    func testGivenAWeatherViewModel_WhenViewDidLoad_ThenNYDescriptionIsCorrectlyReturned() {
+        let expectation = self.expectation(description: "NY City Description is not Correct")
+        let expectedName: String = "Description"
+        sut.nyDescriptionUpdater = { description in
+            XCTAssertEqual(description, expectedName)
+            expectation.fulfill()
+        }
+        sut.viewDidLoad()
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    
+    func testGivenAWeatherViewModel_WhenViewDidLoad_ThenNYTemperatureIsCorrectlyReturned() {
+        let expectation = self.expectation(description: "NY City Temperature is not Correct")
+        let expectedName: String = "99°C"
+        sut.nyTemperatureUpdater = { temperature in
+            XCTAssertEqual(temperature, expectedName)
+            expectation.fulfill()
+        }
+        sut.viewDidLoad()
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    
+    func testGivenAViewModel_WhenACityIsResearched_ThenViewModelIsUpdatedCorrectly() {
+        let mock = MockWeatherNetwork()
+        mock.cityName = "Paris"
+        let expectation = self.expectation(description: "City Network not working")
+        let expectedName = "Paris"
+        sut.cityNameUpdater = { name in
+            XCTAssertEqual(name, expectedName)
+            expectation.fulfill()
+        }
+        sut.getCityWeather(city: "Paris")
+        
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    
+    func testGivenAViewModel_WhenNewYorkIsResearched_ThenViewModelIsUpdatedCorrectly() {
+        let mock = MockWeatherNetwork()
+        mock.cityName = "New York"
+        let expectation = self.expectation(description: "NY Network not working")
+        let expectedName = "New York"
+        sut.nyCityNameUpdater = { name in
+            XCTAssertEqual(name, expectedName)
+            expectation.fulfill()
+        }
+        sut.getNyCityWeather()
+        
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+}
+
+
+
+
+fileprivate final class MockWeatherNetwork: WeatherNetworkType {
+    
+    var cityName = "Roma"
+    var cityTemperature = "99°C"
+    var cityDescription = "Cloudy"
+    
+    func getWeather(city: String, callback: @escaping (Bool, WeatherResponse?) -> Void) {
+        
+    }
+    
+}
+
+
