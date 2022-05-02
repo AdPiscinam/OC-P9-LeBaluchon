3f12adecba0cf097596ff45d48655eab337b8af0
AdPiscinam <lab@walimaloui.com>
2022-05-02 07:30:04 +0200

    Update code and add WeatherNetwork parameter to the View Model

--- OC-P9-LeBaluchon/Weather Flow/Coordinators/CitySelectionCoordinator.swift
+++ OC-P9-LeBaluchon/Weather Flow/Coordinators/CitySelectionCoordinator.swift
@@ -8,27 +8,28 @@ import UIKit
 class CitySelectionCoordinator: Coordinator {
     
     var navigationController: UINavigationController
-    weak var parentCoordinator: WeatherCoordinator?
-    var childCoordinators: [Coordinator] = []
     var viewControllers: [UIViewController] = []
     
+    weak var parentCoordinator: WeatherCoordinator?
+    var childCoordinators: [Coordinator] = []
+
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
 }
 
 extension CitySelectionCoordinator {
-    
     func start() {
         let viewController = CitySelectionViewController()
-        
+        let network = WeatherNetwork()
         viewController.coordinator = self
+        let viewModel = WeatherViewModel(network: network)
+        viewController.viewModel = viewModel
         viewControllers.append(viewController)
         navigationController.navigationBar.prefersLargeTitles = true
         navigationController.present(UINavigationController(rootViewController: viewController), animated: true)
     }
 
- 
     func update(chosenCity: String) {
         parentCoordinator?.update(chosenCity: chosenCity)
     }
