fe5a736c2d83d29fabe9740f574db96449a79d51
AdPiscinam <lab@walimaloui.com>
2022-05-02 07:18:58 +0200

    Update Currency Response

--- OC-P9-LeBaluchon/Converter Flow/Network/ConversionResponse.swift
+++ OC-P9-LeBaluchon/Converter Flow/Network/ConversionResponse.swift
@@ -5,9 +5,29 @@
 
 import Foundation
 
-struct ConversionResponse: Codable {
-    let success: Bool
-    let timestamp: TimeInterval
-    let base, date: String
-    let rates: [String: Double]
+struct CurrencyResponse: Decodable {
+    let baseCurrencyCode: String
+    let baseCurrencyName: String
+    let amount: String
+    let updatedDate: String
+    let rates: [String: Rate]
+    let status: String
+    
+    struct Rate: Decodable {
+        let currencyName: String
+        let rate: String
+        let rateForAmount: String
+        
+        enum CodingKeys: String, CodingKey {
+            case rate
+            case currencyName = "currency_name"
+            case rateForAmount = "rate_for_amount"
+        }
+    }
+    enum CodingKeys: String, CodingKey{
+        case baseCurrencyCode = "base_currency_code"
+        case baseCurrencyName = "base_currency_name"
+        case amount, rates, status
+        case updatedDate = "updated_date"
+      }
 }
