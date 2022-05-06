746d3b3c7d888e4d4cf6464fa121dd7b0f2b3352
AdPiscinam <lab@walimaloui.com>
2022-05-06 16:49:00 +0200

    Create independent File for Cities Structure

--- OC-P9-LeBaluchon/Weather Flow/View Controllers/CitySelectionViewController.swift
+++ OC-P9-LeBaluchon/Weather Flow/View Controllers/CitySelectionViewController.swift
@@ -98,7 +98,7 @@ class CitySelectionViewController: UIViewController, UINavigationControllerDeleg
         
         guard let unwrappedTextLabel = currentCell.textLabel, var unwrappedText = unwrappedTextLabel.text else {
             return
-        }
+        }   
       
         cityNameSearchTextField.text = deleteCountryFrom(name: &unwrappedText)
     }
@@ -177,50 +177,6 @@ extension CitySelectionViewController {
     }
 }
 
-struct Cities: Decodable {
-    
-    let country: String
-    let name: String
-    let lat: String
-    let lng: String
-    
-    
-    static func translationFromJSON(fileName: String) throws -> Data {
-        let bundle = Bundle(for: CitySelectionViewController.self)
-        let url = bundle.url(forResource: "cities", withExtension: "json")!
-        return try Data(contentsOf: url)
-    }
-    
-    static func parseJSON(cityName: String) -> Bool {
-        var bool = false
-        let cities = try! translationFromJSON(fileName: "cities")
-        do {
-            let  result = try JSONDecoder().decode([Cities].self, from: cities)
-            
-            for (_, city) in result.enumerated() {
-                if cityName == city.name {
-                    bool = true
-                }
-            }
-        } catch _ {
-            return false
-        }
-        return bool
-    }
-    
-    static func parseJSON() -> [Cities] {
-        let cities = try! translationFromJSON(fileName: "cities")
-        var parsedCities = [Cities]()
-        do {
-            let  result = try JSONDecoder().decode([Cities].self, from: cities)
-            parsedCities = result
-            return parsedCities
-        } catch _ {
-            print("error in parsing Cities")
-        }
-        return parsedCities
-    }
-}
 
 extension String {
   
